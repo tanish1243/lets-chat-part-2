4e964854ecb2da9415c3af291e88dad30f1be390
tanish1243 <71543868+tanish1243@users.noreply.github.com>
2021-04-16 16:49:56 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,1 +1,12 @@
 
+var firebaseConfig = {
+    apiKey: "AIzaSyBbk657CPROe7uq6MhqUnF4nmwHJKu92pE",
+    authDomain: "letschat2-9cae5.firebaseapp.com",
+    databaseURL: "https://letschat2-9cae5-default-rtdb.firebaseio.com",
+    projectId: "letschat2-9cae5",
+    storageBucket: "letschat2-9cae5.appspot.com",
+    messagingSenderId: "86285751519",
+    appId: "1:86285751519:web:af5acaa9013593263f056a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
